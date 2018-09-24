a57cbea8ebbc41aa04ab20137a1888676394dcf0
Hamidreza <hamidreza@Fatemehs-MacBook-Pro.local>
2018-09-24 14:50:08 -0700

    write points

--- tracker.py
+++ tracker.py
@@ -12,7 +12,7 @@ def frame_list(path):
   for f in folders:
     f1 = glob.glob(path + '/' + f + '/*.tif')
     f1.sort()
-    fns.extend(f1[:-2]) 
+    fns.extend(f1) 
    
   return fns
 
@@ -51,8 +51,7 @@ def get_pts(fn):
   return pnts
  
 
-
-def track(fns, pats):
+def track(fns, pnts):
   # params for ShiTomasi corner detection
   feature_params = dict( maxCorners = 100,
                        qualityLevel = 0.3,
@@ -72,10 +71,13 @@ def track(fns, pats):
   p0 = np.expand_dims(np.array(pnts).astype(np.float32), axis=1) 
   print(p0.shape, len(pnts), p0[1])
   vid = []
+  ppp = []
+  sts = []  
   # Create a mask image for drawing purposes
   c_size = [old_gray.shape[0], old_gray.shape[1], 3]
   mask = np.zeros(c_size, dtype=np.uint8)
   frame = np.zeros(c_size, dtype=np.uint8) 
+  ppp.append(np.array(np.squeeze(p0)))
   for i in range(1, len(fns)):
     print(fns[i])
     frame_gray = ((cv2.imread(fns[i], -1) / 4 )).astype(np.uint8)
@@ -99,9 +101,12 @@ def track(fns, pats):
         break
     # Now update the previous frame and previous points
     old_gray = frame_gray.copy()
-    p0 = good_new.reshape(-1,1,2)
+    p1[st==0] = p1[st==0] * 0 
+    p0 = p1
+    #p0 = good_new.reshape(-1,1,2)
+    print(p0.shape)
+    ppp.append(np.array(np.squeeze(p0)))
   cv2.destroyAllWindows()
-  
   # Define the codec and create VideoWriter object
   fourcc = cv2.VideoWriter_fourcc(*'XVID')
   out = cv2.VideoWriter('output.avi',fourcc, 15.0, (2048, 2048))
@@ -110,57 +115,12 @@ def track(fns, pats):
     # write the flipped frame
     out.write(v)
   out.release()
+  ppp = np.array(ppp) 
+  return ppp
+
 
 fns = frame_list(sys.argv[1]) 
 get_pts(fns[0])
 print(fns)
-track(fns, pnts)
-
-
-
-exit(0)
-
-
-
-
-cap = cv2.VideoCapture('slow.flv')
-# params for ShiTomasi corner detection
-feature_params = dict( maxCorners = 100,
-                       qualityLevel = 0.3,
-                       minDistance = 7,
-                       blockSize = 7 )
-# Parameters for lucas kanade optical flow
-lk_params = dict( winSize  = (15,15),
-                  maxLevel = 2,
-                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
-
-# Take first frame and find corners in it
-ret, old_frame = cap.read()
-old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
-# Create a mask image for drawing purposes
-mask = np.zeros_like(old_frame)
-while(1):
-    ret,frame = cap.read()
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # calculate optical flow
-    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
-    # Select good points
-    good_new = p1[st==1]
-    good_old = p0[st==1]
-    # draw the tracks
-    for i,(new,old) in enumerate(zip(good_new,good_old)):
-        a,b = new.ravel()
-        c,d = old.ravel()
-        mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
-        frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
-    img = cv2.add(frame,mask)
-    cv2.imshow('frame',img)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
-    # Now update the previous frame and previous points
-    old_gray = frame_gray.copy()
-    p0 = good_new.reshape(-1,1,2)
-cv2.destroyAllWindows()
-cap.release()
+ppp = track(fns, pnts)
+np.save(sys.argv[1] + 'ponts.bin', ppp)
